3ac39b75ee124b321a4e9abcc50b77dbf0510a7f
João Pedro <33253735+pjoao266@users.noreply.github.com>
2026-07-07 01:40:42 -0300

    Refactor code structure for improved readability and maintainability

---
+++ laco/.next/dev/static/chunks/[project]_laco_pages__error_0du2_q-._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/node_modules_next_dist_compiled_14ibvna._.js","static/chunks/node_modules_next_dist_shared_lib_12jmhie._.js","static/chunks/node_modules_next_dist_client_16lnmlo._.js","static/chunks/node_modules_next_dist_0zk-zb1._.js","static/chunks/1ybp_next_dist_pages__error_1v1ivkg.js","static/chunks/[next]_entry_page-loader_ts_1m76hjb._.js","static/chunks/node_modules_react-dom_0kla17-._.js","static/chunks/node_modules_10e2-xo._.js","static/chunks/[root-of-the-server]__1h-a1co._.js"],
+    source: "entry"
+});
